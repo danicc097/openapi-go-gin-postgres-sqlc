55d9dbe1bce1e5d1c1821f7bcac0bc616d60e337
Daniel Castro <danicc097@gmail.com>
2022-08-14 12:24:05 +0000

    fix perm

---
+++ postgen/handlers/testdata/merge_changes/got/api_new.go
@@ -0,0 +1,4 @@
+package handlers
+
+// a new generated api file that should be simply copied over from gen to current
+// directory
---
+++ postgen/handlers/testdata/merge_changes/got/api_newapi.go
@@ -0,0 +1,5 @@
+package handlers
+
+// this file would contain e.g. handlers for a renamed tag, or someone
+// didnt follow naming conventions (dont create api_* files).
+// This file should be kept as is.
