337e6926be58f5ddc66b47701a0aa30ca46eb8a6
Daniel Castro <danicc097@gmail.com>
2022-10-16 21:31:30 +0000

    remove testdata output from git

--- internal/postgen/testdata/openapi_generator/merge_changes/got/api_renamedapi.go
+++
@@ -1,5 +0,0 @@
-package rest
-
-// this file would contain e.g. handlers for a renamed tag, or someone
-// didnt follow naming conventions (dont create api_* files).
-// This file should be kept as is.
