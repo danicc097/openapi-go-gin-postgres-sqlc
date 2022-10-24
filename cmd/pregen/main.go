12706c7c36824cedc7d9f1f9758fd6c6863e6fc7
Daniel Castro <danicc097@gmail.com>
2022-10-24 16:25:43 +0000

    pregen script and better xo x function

---
+++ cmd/pregen/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+
+	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal"
+	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/envvar"
+	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/postgen"
+)
+
+func main() {
+	var env, cacheDir, spec string
+
+	flag.StringVar(&env, "env", ".env", "Environment Variables filename")
+	flag.StringVar(&cacheDir, "cachedir", ".postgen.cache", "Cache dir")
+	flag.StringVar(&spec, "spec", "openapi.yaml", "OpenAPI specification")
+	flag.Parse()
+
+	if err := envvar.Load(env); err != nil {
+		log.Fatalf("envvar.Load: %s\n", err)
+	}
+
+	// TODO read openapi spec, look for x-db-enum or x-db-tables vendor ext.
+	// and replace enum values with sql query output
+
+	const baseDir = "internal"
+	conf := &postgen.Conf{
+		CurrentHandlersDir: postgen.Dir(path.Join(baseDir, "rest")),
+		GenHandlersDir:     postgen.Dir(path.Join(baseDir, "gen")),
+		OutHandlersDir:     postgen.Dir(path.Join(baseDir, "rest")),
+		OutServicesDir:     postgen.Dir(path.Join(baseDir, "services")),
+	}
+
+	var stderr bytes.Buffer
+	og := postgen.NewOpenapiGenerator(conf, &stderr, postgen.Dir(cacheDir), spec)
+
+	if err := og.Generate(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, stderr.String())
+		os.Exit(1)
+	}
+
+	url := internal.BuildApiURL("openapi.yaml")
+
+	if err := postgen.SetupSwaggerUI(url); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, stderr.String())
+		os.Exit(1)
+	}
+}
