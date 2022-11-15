d81abe075503a37fddaf83f618c554218e72d6ef
Daniel Castro <danicc097@gmail.com>
2022-11-15 12:18:45 +0000

    gen-schema output without db prefix

--- cmd/gen-schema/main.go
+++ cmd/gen-schema/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -58,13 +59,13 @@ func main() {
 	// we could load the existing spec to reflector.Spec: https://pkg.go.dev/github.com/swaggest/openapi-go/openapi3#example-Spec.UnmarshalYAML
 	// and if "x-db-struct" found in an OPERATION.
 	// NOTE: comments are gone. should print result to openapi.gen.yaml and use that since this is in gen/postgen step
-	schemaBlob, err := os.ReadFile("openapi.yaml")
-	if err != nil {
-		log.Fatalf("openapi spec: %s", err)
-	}
-	if err := reflector.Spec.UnmarshalYAML(schemaBlob); err != nil {
-		log.Fatalf("Spec.UnmarshalYAML: %s", err)
-	}
+	// schemaBlob, err := os.ReadFile("openapi.yaml")
+	// if err != nil {
+	// 	log.Fatalf("openapi spec: %s", err)
+	// }
+	// if err := reflector.Spec.UnmarshalYAML(schemaBlob); err != nil {
+	// 	log.Fatalf("Spec.UnmarshalYAML: %s", err)
+	// }
 
 	// we can edit an existing op by getting all operations with "x-db-struct", trace back to the openapi3.Operation
 	// IMPORTANT:
@@ -72,6 +73,13 @@ func main() {
 	// we can use yq for this (plain replace schema by name) and forget about an openapi.gen.yaml that messes things up.
 	// gen-schema cli should generate a new yaml file with dummy operations (/dummy-$i), i++ while reflector is updated, as we do now (openapi.test.gen.yaml), so **yq reads the schema there, and replaces it in our openapi.yaml**
 	// we dont have to do anything else!
+
+	reflector.InterceptDefName(func(t reflect.Type, defaultDefName string) string {
+		if strings.HasPrefix(defaultDefName, "Db") {
+			return strings.TrimPrefix(defaultDefName, "Db")
+		}
+		return defaultDefName
+	})
 	for i, sn := range structNames {
 		dummyOp := openapi3.Operation{}
 		st, ok := postgen.DbStructs[sn]
@@ -82,37 +90,11 @@ func main() {
 		handleError(reflector.Spec.AddOperation(http.MethodGet, "/dummy-op-"+strconv.Itoa(i), dummyOp))
 		// reflector.Spec.Paths.MapOfPathItemValues["mypath"].MapOfOperationValues["method"].
 	}
-	schema, err := reflector.Spec.MarshalYAML()
+	s, err := reflector.Spec.MarshalYAML()
 	handleError(err)
-	// fmt.Println(string(schema))
 
-	os.WriteFile("openapi.test.gen.yaml", schema, 0o777)
+	fmt.Println(string(s))
+	// os.WriteFile("openapi.test.gen.yaml", schema, 0o777)
 
-	// var s openapi3.Spec
-	// have to merge with our own (done easily but comments are lost. output to openapi.gen.yaml, we are in postgen step so doesnt matter)
-	// have to change frontend generation, swaggerUI paths
-	// schemaBlob, err := os.ReadFile("openapi.yaml")
-	// if err != nil {
-	// 	log.Fatalf("error opening schema file: %s", err)
-	// }
-
-	// oas, err := rest.ReadOpenAPI("openapi.yaml")
-	// if err != nil {
-	// 	log.Fatalf("ReadOpenAPI: %s", err)
-	// }
-	// // oas.Components.SecuritySchemes = kinopenapi3.SecuritySchemes{} // error
-
-	// // fmt.Println(string(schemaBlob))
-	// specWithoutSec, err := oas.MarshalJSON()
-	// if err != nil {
-	// 	log.Fatalf("oas.MarshalJSON: %s", err)
-	// }
-	// // fmt.Println(string(specWithoutSec))
-
-	// if err := s.UnmarshalYAML(specWithoutSec); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(s.Info.Title)
-	// fmt.Println(s.Info.Title)
 	// fmt.Println(s.Components.Schemas.MapOfSchemaOrRefValues["Error"].Schema.Properties["code"].Schema.MapOfAnything["x-foo"])
 }
