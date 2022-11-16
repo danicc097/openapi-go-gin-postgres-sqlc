857b01746f580c190139ce76ab5e8ed45a071359
Daniel Castro <danicc097@gmail.com>
2022-11-16 10:19:13 +0000

    mkcert in traefik for gh actions

--- bin/scripts/install-mkcert.sh
+++ bin/scripts/install-mkcert.sh
@@ -7,6 +7,6 @@ chmod +x mkcert
 sudo mv mkcert /usr/bin/
 source ~/.bashrc
 cd certificates || exit
-mkcert --cert-file localhost.pem --key-file localhost-key.pem localhost 127.0.0.1 ::1 host.docker.internal
+mkcert --cert-file localhost.pem --key-file localhost-key.pem localhost "*.dev.localhost" "*.ci.localhost" "*.prod.localhost" 127.0.0.1 ::1 host.docker.internal
 cd ..
 mkcert -install
