a8fed91db06174ca3b14a25fa9184a82865bd917
Daniel Castro <danicc097@gmail.com>
2022-11-25 18:36:07 +0000

    BROKEN gen-schema: trying to generate openapi-go from reflect struct

--- cmd/gen-schema/main.go
+++ cmd/gen-schema/main.go
@@ -82,16 +82,37 @@ func main() {
 			if t == "" {
 				log.Fatalf("field %v in struct-name %s does not have an `openapi-json` tag", field, sn)
 			}
-			field.Tag = reflect.StructTag(fmt.Sprintf("json=\"%s\"", t))
+			field.Tag = reflect.StructTag(fmt.Sprintf("json:\"%s\"", t))
 			fields = append(fields, field)
+			x, _ := field.Tag.Lookup("json")
+			fmt.Printf("field.Tag json is: %#v\n", x)
 		}
 		newSt := reflect.StructOf(fields)
-		fmt.Printf("reflect.New(newSt): %#v\n", reflect.New(newSt))
 
-		// output: {00000000-0000-0000-0000-000000000000   <nil> <nil> <nil>  <nil> [] 0 0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC <nil> <nil> <nil> <nil>}
+		// output: struct .... {UserID:uuid.UUID{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}, Username:"", Email:"", FirstName:(*string)(nil), LastName:(*string)(nil), FullName:(*string)(nil), ExternalID:"", APIKeyID:(*int)(nil), Scopes:[]string(nil), RoleRank:0, CreatedAt:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC), UpdatedAt:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC), DeletedAt:<nil>, TimeEntries:(*[]db.TimeEntry)(nil), Teams:(*[]db.Team)(nil), WorkItems:(*[]db.WorkItem)(nil)}
 		handleError(reflector.SetJSONResponse(&dummyOp, reflect.Indirect(reflect.New(newSt)), http.StatusTeapot))
-		// output: {00000000-0000-0000-0000-000000000000   <nil> <nil> <nil>  <nil> [] 0 0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC <nil> <nil> <nil> <nil> false false} // last booleans xo "_XXX" fields
+		// output: db.User{UserID:uuid.UUID{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}, Username:"", Email:"", FirstName:(*string)(nil), LastName:(*string)(nil), FullName:(*string)(nil), ExternalID:"", APIKeyID:(*int)(nil), Scopes:[]string(nil), RoleRank:0, CreatedAt:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC), UpdatedAt:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC), DeletedAt:<nil>, TimeEntries:(*[]db.TimeEntry)(nil), Teams:(*[]db.Team)(nil), WorkItems:(*[]db.WorkItem)(nil), _exists:false, _deleted:false}
+		// last booleans xo "_XXX" fields
 		// handleError(reflector.SetJSONResponse(&dummyOp, st, http.StatusTeapot))
+		// custom struct
+		// handleError(reflector.SetJSONResponse(&dummyOp, struct {
+		// 	UserID      uuid.UUID       `json:"userID"`
+		// 	Username    string          `json:"username"`
+		// 	Email       string          `json:"email"`
+		// 	FirstName   *string         `json:"firstName"`
+		// 	LastName    *string         `json:"lastName"`
+		// 	FullName    *string         `json:"fullName"`
+		// 	ExternalID  string          `json:"externalID"`
+		// 	APIKeyID    *int            `json:"apiKeyID"`
+		// 	Scopes      []string        `json:"scopes"`
+		// 	RoleRank    int16           `json:"roleRank"`
+		// 	CreatedAt   time.Time       `json:"createdAt"`
+		// 	UpdatedAt   time.Time       `json:"updatedAt"`
+		// 	DeletedAt   *time.Time      `json:"deletedAt"`
+		// 	TimeEntries *[]db.TimeEntry `json:"timeEntries"`
+		// 	Teams       *[]db.Team      `json:"teams"`
+		// 	WorkItems   *[]db.WorkItem  `json:"workItems"`
+		// }{}, http.StatusTeapot))
 		fmt.Printf("dummyOp: %#v\n", dummyOp)
 		reflector.Spec.Components.Schemas.MapOfSchemaOrRefValues[sn].Schema.MapOfAnything = map[string]interface{}{"x-db-struct": sn}
 		handleError(reflector.Spec.AddOperation(http.MethodGet, "/dummy-op-"+strconv.Itoa(i), dummyOp))
