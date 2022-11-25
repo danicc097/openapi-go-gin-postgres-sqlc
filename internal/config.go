76f3300b158a5606e49e839af877c8a10e04c370
Daniel Castro <danicc097@gmail.com>
2022-11-25 13:46:46 +0000

    notes on json app config coupled with .env

--- internal/config.go
+++ internal/config.go
@@ -11,12 +11,50 @@ import (
 	"github.com/pkg/errors"
 )
 
-// AppConfig contains app settings which are read from a config file.
+type OIDCConfig struct {
+	ClientID     string  `json:"clientId" validate:"required"`
+	ClientSecret string  `json:"clientSecret" validate:"required"`
+	Domain       string  `json:"domain" validate:"required"`
+	Issuer       string  `json:"issuer" validate:"required"`
+	Scopes       string  `json:"scopes" validate:"required"`
+	ServerPort   *string `json:"serverPort"`
+}
+
+type PostgresConfig struct {
+	Port     string `json:"-" validate:"required"`
+	User     string `json:"-" validate:"required"`
+	Password string `json:"-" validate:"required"`
+	Server   string `json:"-" validate:"required"`
+	Database string `json:"-" validate:"required"`
+}
+
+// TODO frontend still needs .env.<env> for dynamic config.json
+// without rebuilding.
+// Postgres and backend also need .env shared.
+// We could have DBConfig struct with json:"-"
+// and on startup fill it in with whatever is in the current env,
+// then validate it as usual.
+// this way postgres can reuse the .env.* as usual
+// and get typed config
+// ^ do the same for .env values that are shared with frontend:
+// TDLR whatever appears in .env.template must have json:"-" and env is loaded to the struct after unmarshalling config/*.json
+
+// sharing config between packages:
+// os.getenv is very convenient.
+// the closest we can get: internal.Config() returns the already built and validated *config.
+// returning a copy every single time is too bad. its an internal package so
+// just need to ensure nothing silly is being done like overwriting it...
+
+// AppConfig contains app settings which are read from a config file. Excluded fields from JSON are read from environment variables.
 type AppConfig struct {
-	Test              string    `json:"test"`
-	TestOptional      *string   `json:"testOptional"`
-	TestArray         []string  `json:"testArray"`
-	TestArrayOptional *[]string `json:"testArrayOptional"`
+	Postgres   PostgresConfig `json:"-" validate:"required"`
+	OIDC       OIDCConfig     `json:"oidc" validate:"required"`
+	SigningKey string         `json:"signingKey" validate:"required"`
+}
+
+func (ac *AppConfig) Validate() error {
+	// see https://github.com/go-playground/validator/blob/master/_examples/struct-level/main.go and other examples
+	return nil
 }
 
 // Returns the directory of the file this function lives in.
