4ac0524001c177f95b9e1efc4954b7a133488ecb
Daniel Castro <danicc097@gmail.com>
2022-12-03 21:30:18 +0000

    TODO fix goroutine and closure shenanigans

--- internal/rest/api.go
+++ internal/rest/api.go
@@ -1,9 +1,6 @@
 package rest
 
 import (
-	"fmt"
-	"time"
-
 	v1 "github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/pb/python-ml-app-protos/tfidf/v1"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/services"
 	"github.com/gin-gonic/gin"
@@ -34,19 +31,19 @@ func NewHandlers(
 	authnsvc *services.Authentication,
 	authmw *authMiddleware,
 ) *Handlers {
-	stream := newSSEServer()
+	// stream := newSSEServer()
 
-	// must be called just once
-	go func() {
-		for {
-			// We are streaming current time to clients in the interval 10 seconds
-			time.Sleep(time.Second * 2)
-			now := time.Now().Format("2006-01-02 15:04:05")
-			currentTime := fmt.Sprintf("The Current Time Is %v", now)
+	// // must be called just once
+	// go func() {
+	// 	for {
+	// 		// We are streaming current time to clients in the interval 10 seconds
+	// 		time.Sleep(time.Second * 2)
+	// 		now := time.Now().Format("2006-01-02 15:04:05")
+	// 		currentTime := fmt.Sprintf("The Current Time Is %v", now)
 
-			stream.Message <- currentTime
-		}
-	}()
+	// 		stream.Message <- currentTime
+	// 	}
+	// }()
 
 	return &Handlers{
 		logger:         logger,
@@ -56,7 +53,7 @@ func NewHandlers(
 		authzsvc:       authzsvc,
 		authnsvc:       authnsvc,
 		authmw:         authmw,
-		stream:         stream,
+		// stream:         stream,
 	}
 }
 
@@ -65,7 +62,9 @@ func (h *Handlers) middlewares(opID OperationID) []gin.HandlerFunc {
 	switch opID {
 	case Events:
 		// return []gin.HandlerFunc{}
-		return []gin.HandlerFunc{SSEHeadersMiddleware(), h.stream.serveHTTP()}
+		return []gin.HandlerFunc{SSEHeadersMiddleware()}
+		// does not work
+		// return []gin.HandlerFunc{SSEHeadersMiddleware(), h.stream.serveHTTP()}
 	default:
 		return []gin.HandlerFunc{}
 	}
