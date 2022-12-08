1eff8b29c41b4c227531a6412716f57d40916690
Daniel Castro <danicc097@gmail.com>
2022-12-08 18:02:47 +0000

    remove redux gen - notes on auth header

--- frontend/src/hooks/auth/useAuthenticatedUser.tsx
+++ frontend/src/hooks/auth/useAuthenticatedUser.tsx
@@ -1,30 +1,27 @@
 import { shallowEqual } from 'react-redux'
-import { useAppDispatch, useAppSelector } from 'src/redux/hooks'
 import { capitalize } from 'lodash'
-import type { UserResponse } from 'src/redux/slices/gen/internalApi'
 import { useMemo } from 'react'
 import { COLOR_BLIND_PALETTE } from 'src/utils/colors'
 import roles from '@roles'
 import scopes from '@scopes'
+import type { UserResponse } from 'src/gen/model'
 export const useAuthenticatedUser = () => {
-  const dispatch = useAppDispatch()
-
-  // const user = useAppSelector((state) => state.auth.user, shallowEqual)
-  const user: UserResponse = useMemo(
-    () =>
-      ({
-        email: 'test@mail.com',
-        first_name: 'John',
-        last_name: 'Doe',
-        role_rank: roles.user.rank,
-        full_name: 'John Doe',
-        username: 'john.doe',
-        scopes: ['users:read', 'test-scope', 'scopes:write'],
-        createdAt: Date.now(),
-        deletedAt: null,
-      } as UserResponse),
-    [],
-  )
+  // TODO for app_env dev, remove Authorization header and comes from backend via x-api-key header
+  // or have fallthorugh if authentication failed instead - would need multierror
+  const user: UserResponse = {
+    hasGlobalNotifications: true,
+    hasPersonalNotifications: true,
+    role: 'user',
+    userID: crypto.randomUUID(),
+    email: 'test@mail.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    fullName: 'John Doe',
+    username: 'john.doe',
+    scopes: ['users:read', 'test-scope', 'scopes:write'],
+    createdAt: new Date(),
+    deletedAt: null,
+  }
   const avatarColor =
     COLOR_BLIND_PALETTE[capitalize(user?.email).charCodeAt(0) % COLOR_BLIND_PALETTE.length] || '#1060e0'
 
@@ -32,16 +29,7 @@ export const useAuthenticatedUser = () => {
     null
   }
 
-  const registerNewUser = ({ username, email, password }) => {
-    null
-  }
-  const requestUserLogin = ({ email, password }) => {
-    null
-  }
-
   return {
-    registerNewUser,
-    requestUserLogin,
     user,
     avatarColor,
     logUserOut,
