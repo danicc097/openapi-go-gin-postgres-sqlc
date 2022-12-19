4217ac389b4333bc4b7cd0d398766b3e18c18d0c
Daniel Castro <danicc097@gmail.com>
2022-12-19 12:02:17 +0000

    isauthorized

--- frontend/src/services/authorization.ts
+++ frontend/src/services/authorization.ts
@@ -1,6 +1,26 @@
 import roles from '@roles'
 import scopes from '@scopes'
+import type { Role, Scopes, UserResponse } from 'src/gen/model'
 
-export function isAuthorized(): boolean {
+interface IsAuthorizedParams {
+  user: UserResponse
+  requiredRole?: Role
+  requiredScopes?: Scopes
+}
+
+export function isAuthorized({ user, requiredRole = null, requiredScopes = null }: IsAuthorizedParams): boolean {
+  if (requiredRole !== null) {
+    if (roles[user.role].rank < roles[requiredRole].rank) {
+      return false
+    }
+  }
+  if (requiredScopes !== null) {
+    for (const scope of requiredScopes) {
+      if (!user.scopes.includes(scope)) {
+        return false
+      }
+    }
+  }
+
   return true
 }
