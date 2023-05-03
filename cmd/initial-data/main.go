af627a2ce71133b1c93736ab4152c17045483ca0
Daniel Castro <danicc097@gmail.com>
2023-05-03 18:52:17 +0000

    initial data refactor

--- cmd/initial-data/main.go
+++ cmd/initial-data/main.go
@@ -8,15 +8,25 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/envvar"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/models"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos/postgresql"
+	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos/postgresql/gen/db"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/services"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/utils/pointers"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
 
+var (
+	logger, _ = zap.NewDevelopment()
+	pool      *pgxpool.Pool
+)
+
 func main() {
+	var err error
 	var env, scopePolicyPath, rolePolicyPath string
 
 	flag.StringVar(&env, "env", ".env", "Environment Variables filename")
@@ -28,8 +38,7 @@ func main() {
 		log.Fatalf("envvar.Load: %s\n", err)
 	}
 
-	logger, _ := zap.NewDevelopment()
-	pool, _, err := postgresql.New(logger)
+	pool, _, err = postgresql.New(logger)
 	if err != nil {
 		log.Fatalf("postgresql.New: %s\n", err)
 	}
@@ -44,20 +53,31 @@ func main() {
 	wiTagRepo := postgresql.NewWorkItemTag()
 
 	authzSvc, err := services.NewAuthorization(logger, scopePolicyPath, rolePolicyPath)
-	if err != nil {
-		logger.Sugar().Fatalf("NewAuthorization: %s\n", err)
-	}
+	handleError(err)
 
 	userSvc := services.NewUser(logger, userRepo, notifRepo, authzSvc)
 	authnSvc := services.NewAuthentication(logger, userSvc, pool)
 	/*activitySvc :=*/ _ = services.NewActivity(logger, activityRepo)
-	/*teamSvc :=*/ _ = services.NewTeam(logger, teamRepo)
+	teamSvc := services.NewTeam(logger, teamRepo)
 	/*projectSvc :=*/ _ = services.NewProject(logger, projectRepo, teamRepo)
 	/*teSvc :=*/ _ = services.NewTimeEntry(logger, teRepo)
 	/*wiTypeSvc :=*/ _ = services.NewWorkItemType(logger, wiTypeRepo)
 	/*wiTagSvc :=*/ _ = services.NewWorkItemTag(logger, wiTagRepo)
 
 	ctx := context.Background()
+
+	if env == "prod" {
+		// prod specific init
+		os.Exit(0)
+	}
+
+	/**
+	 *
+	 * USERS
+	 *
+	 **/
+
+	var userIDs []uuid.UUID
 
 	logger.Sugar().Info("Registering users...")
 	for i := 0; i < 10; i++ {
@@ -67,15 +87,12 @@ func main() {
 			Email:      "user_" + strconv.Itoa(i) + "@mail.com",
 			ExternalID: "external_id_user_" + strconv.Itoa(i),
 		})
-		if err != nil {
-			logger.Sugar().Fatalf("Could not register user: %s", err)
-		}
+		handleError(err)
 		_, err = authnSvc.CreateAPIKeyForUser(ctx, u)
-		if err != nil {
-			logger.Sugar().Fatalf("Could not create api key for user: %s", err)
-		}
+		handleError(err)
 
-		logger.Sugar().Info("Registered", u.Username)
+		logger.Sugar().Info("Registered ", u.Username)
+		userIDs = append(userIDs, u.UserID)
 	}
 	u, err := userSvc.Register(ctx, pool, services.UserRegisterParams{
 		Username:   "manager_1",
@@ -84,14 +101,12 @@ func main() {
 		ExternalID: "external_id_manager_1",
 		Role:       models.RoleManager,
 	})
-	if err != nil {
-		logger.Sugar().Fatalf("Could not register user: %s", err)
-	}
+	handleError(err)
 	_, err = authnSvc.CreateAPIKeyForUser(ctx, u)
-	if err != nil {
-		logger.Sugar().Fatalf("Could not create api key for user: %s", err)
-	}
-	logger.Sugar().Info("Registered", u.Username)
+	handleError(err)
+	logger.Sugar().Info("Registered ", u.Username)
+	userIDs = append(userIDs, u.UserID)
+
 	u, err = userSvc.Register(ctx, pool, services.UserRegisterParams{
 		Username:   "superadmin_1",
 		FirstName:  pointers.New("MrSuperadmin"),
@@ -99,18 +114,36 @@ func main() {
 		ExternalID: "external_id_superadmin_1",
 		Role:       models.RoleSuperAdmin,
 	})
-	if err != nil {
-		logger.Sugar().Fatalf("Could not register user: %s", err)
-	}
+	handleError(err)
 	_, err = authnSvc.CreateAPIKeyForUser(ctx, u)
-	if err != nil {
-		logger.Sugar().Fatalf("Could not create api key for user: %s", err)
-	}
-	logger.Sugar().Info("Registered", u.Username)
+	handleError(err)
+	logger.Sugar().Info("Registered ", u.Username)
+	userIDs = append(userIDs, u.UserID)
+
+	/**
+	 *
+	 * TEAMS
+	 *
+	 **/
+
+	t1, err := teamSvc.Create(ctx, pool, &db.TeamCreateParams{
+		ProjectID:   internal.ProjectIDByName[models.ProjectDemo],
+		Name:        "Team 1",
+		Description: "Team 1 description",
+	})
+	handleError(err)
+	fmt.Printf("t1: %v\n", t1)
+	// TODO usvc.AssignTeam(userIDs[0], t1.TeamID)
 }
 
 func errAndExit(out []byte, err error) {
 	fmt.Fprintf(os.Stderr, "combined out:\n%s\n", string(out))
 	fmt.Fprintf(os.Stderr, "cmd.Run() failed with %s\n", err)
 	os.Exit(1)
+}
+
+func handleError(err error) {
+	if err != nil {
+		logger.Sugar().Fatalf("error: %s", err)
+	}
 }
