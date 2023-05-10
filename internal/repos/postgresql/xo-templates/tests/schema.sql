1c308dc25fb825cacbadf98b70c41578366fd193
Daniel Castro <danicc097@gmail.com>
2023-05-10 13:06:18 +0000

    update sql linting and xo schema

--- internal/repos/postgresql/xo-templates/tests/schema.sql
+++ internal/repos/postgresql/xo-templates/tests/schema.sql
@@ -1,4 +1,5 @@
 create schema if not exists extensions;
+
 create schema if not exists xo_tests;
 
 create table xo_tests.users (
@@ -40,4 +41,37 @@ comment on column xo_tests.book_reviews.reviewer is '"cardinality":M2O';
 
 comment on column xo_tests.book_reviews.book_id is '"cardinality":M2O';
 
--- TODO: dummy data for tests. just exec this file on test setup
+do $BODY$
+declare
+  user_1_id uuid;
+  user_2_id uuid;
+begin
+  insert into xo_tests.users (name , created_at)
+    values ('John Doe' , current_timestamp)
+  returning
+    user_id into user_1_id;
+  -- PERFORM pg_sleep(0.5); -- not working for some reason
+  insert into xo_tests.users (name , created_at)
+    values ('Jane Smith' , current_timestamp + '1 h')
+  returning
+    user_id into user_2_id;
+
+  insert into xo_tests.books (name)
+    values ('Book 1');
+  insert into xo_tests.books (name)
+    values ('Book 2');
+
+  insert into xo_tests.book_authors (book_id , author_id)
+    values (1 , user_1_id);
+  insert into xo_tests.book_authors (book_id , author_id , pseudonym)
+    values (1 , user_2_id , 'not Jane Smith');
+  insert into xo_tests.book_authors (book_id , author_id)
+    values (2 , user_2_id);
+
+  insert into xo_tests.book_reviews (book_id , reviewer)
+    values (1 , user_1_id);
+  insert into xo_tests.book_reviews (book_id , reviewer)
+    values (2 , user_2_id);
+end;
+$BODY$
+language plpgsql;
