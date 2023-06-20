580467a6829dfd40f8fef94816aa5d6c12927448
Daniel Castro <danicc097@gmail.com>
2023-06-20 21:10:39 +0000

    refactor

--- frontend/src/utils/jsonSchema.ts
+++ frontend/src/utils/jsonSchema.ts
@@ -12,22 +12,26 @@ type SchemaType = {
   isArray?: boolean
 }
 
-export function extractFieldTypes(schema: JsonSchemaField): {
-  [key: string]: { type: string; required: boolean; isArray?: boolean }
-} {
+type FormGeneratorFields = {
+  [key: string]: {
+    type: string
+    required: boolean
+    isArray?: boolean
+  }
+}
+
+export function extractFieldTypes(schema: JsonSchemaField): FormGeneratorFields {
   const result: { [key: string]: SchemaType } = {}
 
-  function traverseSchema(field: JsonSchemaField, currentPath: string, parentRequired: boolean, parentArray = false) {
+  function traverseSchema(field: JsonSchemaField, currentPath: string, parentArray = false, parent?: JsonSchemaField) {
     const requiredFields = new Set(field.required || [])
-    const isRequiredByParent = parentRequired || requiredFields.size > 0
 
-    const isFieldRequired = isRequiredByParent && !requiredFields.has(currentPath)
+    const isFieldRequired = !requiredFields.has(currentPath)
     if (currentPath !== '') {
       if (field.format) {
         result[currentPath] = { type: field.format, required: isFieldRequired, isArray: parentArray }
       } else if (field.type) {
-        const types = Array.isArray(field.type) ? field.type.filter((type) => type !== 'null') : [field.type]
-        const type = types.filter((t) => t !== 'null')[0]
+        const type = filterType(field)
         const isArray = Array.isArray(field.type) || parentArray
         result[currentPath] = { type, required: isFieldRequired, isArray }
       }
@@ -36,18 +40,29 @@ export function extractFieldTypes(schema: JsonSchemaField): {
     if (field.properties) {
       for (const key in field.properties) {
         const newPath = currentPath ? `${currentPath}.${key}` : key
-        traverseSchema(field.properties[key], newPath, isRequiredByParent)
+        traverseSchema(field.properties[key], newPath, false, field)
       }
     }
 
     if (field.items && field.items.type === 'object') {
       const newPath = currentPath
-      traverseSchema(field.items, newPath, isRequiredByParent, true)
-      result[newPath] = { type: 'arrayOfObject', required: isRequiredByParent, isArray: true }
+      traverseSchema(field.items, newPath, true, field)
+      result[newPath] = { type: 'arrayOfObject', required: isFieldRequired, isArray: true }
+    }
+
+    if (parent?.type === 'array' && field.items?.type && !field.items.properties) {
+      const newPath = currentPath
+      const isArray = true
+      const type = filterType(field.items)
+      result[newPath] = { type, required: isFieldRequired, isArray }
     }
   }
 
   traverseSchema(schema, '', false)
 
   return result
+}
+function filterType(field: JsonSchemaField) {
+  // TODO return format if exists
+  return Array.isArray(field.type) ? field.type.filter((type) => type !== 'null')[0] : field.type
 }
