b3587f9529e0f397aeacdd1ff22f910e086eb06e
Daniel Castro <danicc097@gmail.com>
2023-07-19 22:15:14 +0000

    schema fields gen

--- frontend/scripts/generate-schema-fields.js
+++ frontend/scripts/generate-schema-fields.js
@@ -1,12 +1,26 @@
 import JSON_SCHEMA from '../src/client-validator/gen/dereferenced-schema.json' assert { type: 'json' }
 
-// TODO: return object indexed by projectName and save to workItemSchemaFields. will be used by front and back.
-// instead of a global config mess we can have per-user config in local storage for visualization.
-// simple if (!projects.includes(<project>)) guard sanity check will do it
 const projects = JSON_SCHEMA.definitions.Project.enum
-const schemaFields = parseSchemaFields(JSON_SCHEMA.definitions.RestDemoWorkItemsResponse)
+
+const workItemResponses = {
+  demo: JSON_SCHEMA.definitions.RestDemoWorkItemsResponse,
+  demo_two: JSON_SCHEMA.definitions.RestDemoTwoWorkItemsResponse,
+}
+
+const schemaFields = Object.entries(workItemResponses).reduce((acc, [project, schema]) => {
+  if (!projects.includes(project))
+    throw new Error(`Project '${project}' does not exist. Existing projects: ${projects}`)
+
+  acc[project] = parseSchemaFields(schema)
+
+  return acc
+}, {})
+
+// instead of a global config mess we can have per-user config in local storage for visualization.
+// and get rid of current project.boardConfig fields (will be used for other actually useful generic settings)
 console.log(schemaFields)
 
+// adapted from src/utils/jsonSchema.ts
 function parseSchemaFields(schema) {
   const schemaFields = {}
 
