e7cfaa7bf050a1b3b7c75917d84d9af79babd32e
Daniel Castro <danicc097@gmail.com>
2023-07-20 07:58:03 +0000

    notes for saner global visualization config

--- frontend/scripts/generate-schema-fields.js
+++ frontend/scripts/generate-schema-fields.js
@@ -1,71 +1,14 @@
 import JSON_SCHEMA from '../src/client-validator/gen/dereferenced-schema.json' assert { type: 'json' }
 
-const projects = JSON_SCHEMA.definitions.Project.enum
-
 const workItemResponses = {
   demo: JSON_SCHEMA.definitions.RestDemoWorkItemsResponse,
   demo_two: JSON_SCHEMA.definitions.RestDemoTwoWorkItemsResponse,
 }
 
-const schemaFields = Object.entries(workItemResponses).reduce((acc, [project, schema]) => {
-  if (!projects.includes(project))
-    throw new Error(`Project '${project}' does not exist. Existing projects: ${projects}`)
-
-  acc[project] = parseSchemaFields(schema)
-
-  return acc
-}, {})
-
-// instead of a global config mess we can have per-user config in local storage for visualization.
-// and get rid of current project.boardConfig fields (will be used for other actually useful generic settings)
-console.log(schemaFields)
-
-// adapted from src/utils/jsonSchema.ts
-function parseSchemaFields(schema) {
-  const schemaFields = {}
-
-  function traverseSchema(obj, path = []) {
-    const isArrayOfObj = extractType(obj) === 'object' && !!obj.type?.includes('array') && obj.items?.properties
-    const isObj = obj.properties && extractType(obj) === 'object'
-
-    if (isArrayOfObj) {
-      extract(obj?.items?.properties)
-    } else if (isObj) {
-      extract(obj.properties)
-    }
-
-    function extract(properties) {
-      for (const key in properties) {
-        const newPath = [...path, key]
-        const property = properties[key]
-        if (!property) {
-          continue
-        }
-
-        schemaFields[newPath.join('.')] = null
-        traverseSchema(property, newPath)
-      }
-    }
-  }
-
-  traverseSchema(schema)
-
-  return Object.keys(schemaFields)
-}
-
-function extractType(obj) {
-  const type = _type(obj.type)
-  if (type === 'array') {
-    if (obj?.items?.type === 'object') {
-      return 'object'
-    } else {
-      return _type(obj?.items?.type)
-    }
-  }
-
-  return obj.format ?? type
-
-  function _type(x) {
-    return Array.isArray(x) ? x.filter((t) => t !== 'null')[0] : x
-  }
-}
+/**
+ * Instead of backend merging and setting defualt, since work item response is just for visualization, ie frontend, have a dedicated projects.board_config->"visualization.workItem"
+ * but backend doesnt use for anything. when saving config, it just overrides postgres jsonb field at "visualization.workItem" path.
+ * We also have audit table in case someone messes up, we could have history option to restore.
+ *
+ * ANy user can also have its own visualization config with overrides based on the current defaults, saved to LS instead
+ */
