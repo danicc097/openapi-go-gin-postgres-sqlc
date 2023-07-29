0f65bff645e06a46ac4f3fc9c6697f4ecf2073fc
Daniel Castro <danicc097@gmail.com>
2023-07-29 10:57:37 +0000

    playwright frontend path baseurl

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test'
-
+import CONFIG from "../frontend/config.json"
+import { frontendPath } from 'utils/path'
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -24,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: frontendPath(null),
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
---
+++ e2e/utils/path.ts
@@ -0,0 +1,11 @@
+import CONFIG from '../../frontend/config.json'
+
+export function apiPath(path: string | null) {
+  const port = CONFIG.API_PORT?.length > 0 ? `:${CONFIG.API_PORT}` : ''
+  return `https://${CONFIG.DOMAIN}${port}${CONFIG.API_PREFIX}${CONFIG.API_VERSION}${path ?? ''}`
+}
+
+export function frontendPath(path: string | null) {
+  const port = CONFIG.API_PORT?.length > 0 ? `:${CONFIG.API_PORT}` : ''
+  return `https://${CONFIG.DOMAIN}${port}${path ?? ''}`
+}
