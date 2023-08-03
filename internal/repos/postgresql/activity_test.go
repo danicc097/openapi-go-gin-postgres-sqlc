76463a1e82c9b82683854ffea61e175d95a8333a
Daniel Castro <danicc097@gmail.com>
2023-08-03 16:41:36 +0000

    tests update

--- internal/repos/postgresql/activity_test.go
+++ internal/repos/postgresql/activity_test.go
@@ -2,8 +2,10 @@ package postgresql_test
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
+	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/models"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos/postgresql"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos/postgresql/gen/db"
@@ -30,114 +32,44 @@ func TestActivity_ByIndexedQueries(t *testing.T) {
 		t.Fatalf("activityRepo.Create unexpected error = %v", err)
 	}
 
-	type argsString struct {
-		filter    string
-		projectID int
-		fn        func(context.Context, db.DBTX, string, int, ...db.ActivitySelectConfigOption) (*db.Activity, error)
-	}
-
-	testString := []struct {
-		name string
-		args argsString
-	}{
+	uniqueTestCases := []filterTestCase[*db.Activity]{
 		{
 			name: "name",
-			args: argsString{
-				filter:    activity.Name,
-				projectID: activity.ProjectID,
-				fn:        (activityRepo.ByName),
+			filter: []any{
+				activity.Name,
+				internal.ProjectIDByName[models.ProjectDemo],
+			},
+			repoMethod: reflect.ValueOf(activityRepo.ByName),
+			callback: func(t *testing.T, res *db.Activity) {
+				assert.Equal(t, res.Name, activity.Name)
 			},
 		},
-	}
-	for _, tc := range testString {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			foundActivity, err := tc.args.fn(context.Background(), testPool, tc.args.filter, tc.args.projectID)
-			if err != nil {
-				t.Fatalf("unexpected error = %v", err)
-			}
-			assert.Equal(t, foundActivity.ActivityID, activity.ActivityID)
-		})
-
-		t.Run(tc.name+" - no rows when record does not exist", func(t *testing.T) {
-			t.Parallel()
-
-			errContains := errNoRows
-
-			filter := "inexistent activity"
-
-			_, err := tc.args.fn(context.Background(), testPool, filter, tc.args.projectID)
-			if err == nil {
-				t.Fatalf("expected error = '%v' but got nothing", errContains)
-			}
-			assert.ErrorContains(t, err, errContains)
-		})
-	}
-
-	type argsInt struct {
-		filter int
-		fn     func(context.Context, db.DBTX, int, ...db.ActivitySelectConfigOption) (*db.Activity, error)
-	}
-	testsInt := []struct {
-		name string
-		args argsInt
-	}{
 		{
-			name: "activity_id",
-			args: argsInt{
-				filter: activity.ActivityID,
-				fn:     (activityRepo.ByID),
+			name:       "id",
+			filter:     activity.ActivityID,
+			repoMethod: reflect.ValueOf(activityRepo.ByID),
+			callback: func(t *testing.T, res *db.Activity) {
+				assert.Equal(t, res.ActivityID, activity.ActivityID)
 			},
 		},
 	}
-	for _, tc := range testsInt {
+	for _, tc := range uniqueTestCases {
 		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			foundActivity, err := tc.args.fn(context.Background(), testPool, tc.args.filter)
-			if err != nil {
-				t.Fatalf("unexpected error = %v", err)
-			}
-			assert.Equal(t, foundActivity.ActivityID, activity.ActivityID)
-		})
-
-		t.Run(tc.name+" - no rows when record does not exist", func(t *testing.T) {
-			t.Parallel()
-
-			errContains := errNoRows
-
-			filter := 254364 // does not exist
-
-			_, err := tc.args.fn(context.Background(), testPool, filter)
-			if err == nil {
-				t.Fatalf("expected error = '%v' but got nothing", errContains)
-			}
-			assert.ErrorContains(t, err, errContains)
-		})
+		runGenericFilterTests(t, tc)
 	}
 
-	t.Run("project_id", func(t *testing.T) {
-		t.Parallel()
-
-		foundActivity, err := activityRepo.ByProjectID(context.Background(), testPool, activity.ProjectID)
-		if err != nil {
-			t.Fatalf("unexpected error = %v", err)
-		}
-		assert.Equal(t, foundActivity[0].ProjectID, activity.ProjectID)
-	})
-
-	t.Run("project_id"+" - no rows when record does not exist", func(t *testing.T) {
-		t.Parallel()
-
-		filter := 254364 // does not exist
-
-		aa, err := activityRepo.ByProjectID(context.Background(), testPool, filter)
-		if err != nil {
-			t.Fatalf("unexpected error = %v", err)
-		}
-		assert.Len(t, aa, 0)
-	})
+	nonUniqueTestCases := []filterTestCase[[]db.Activity]{
+		{
+			name:       "project_id",
+			filter:     internal.ProjectIDByName[models.ProjectDemo],
+			repoMethod: reflect.ValueOf(activityRepo.ByProjectID),
+			callback: func(t *testing.T, res []db.Activity) {
+				assert.Equal(t, res[0].ProjectID, internal.ProjectIDByName[models.ProjectDemo])
+			},
+		},
+	}
+	for _, tc := range nonUniqueTestCases {
+		tc := tc
+		runGenericFilterTests(t, tc)
+	}
 }
