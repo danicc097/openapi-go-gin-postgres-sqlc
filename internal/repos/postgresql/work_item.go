cd89ba49b0c14d64505b43c8c80aa5d20f843ddb
Daniel Castro <danicc097@gmail.com>
2023-08-04 20:08:21 +0000

    notes on shared work item repo

--- internal/repos/postgresql/work_item.go
+++ internal/repos/postgresql/work_item.go
@@ -2,6 +2,7 @@ package postgresql
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos"
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos/postgresql/gen/db"
@@ -13,6 +14,10 @@ type WorkItem struct {
 }
 
 // NewWorkItem instantiates the WorkItem repository.
+// NOTE: maybe we can consider work item an aggregate root, since we don't
+// need distinction between projects for some tasks like assigning members, tags, generic
+// functionality like Delete, Restore...
+// and this simplifies everything a lot.
 func NewWorkItem() *WorkItem {
 	return &WorkItem{
 		q: db.New(),
@@ -23,4 +28,40 @@ var _ repos.WorkItem = (*WorkItem)(nil)
 
 func (u *WorkItem) ByID(ctx context.Context, d db.DBTX, id int, opts ...db.WorkItemSelectConfigOption) (*db.WorkItem, error) {
 	return db.WorkItemByWorkItemID(ctx, d, id, opts...)
+}
+
+func (u *WorkItem) AssignMember(ctx context.Context, d db.DBTX, params *db.WorkItemAssignedUserCreateParams) error {
+	_, err := db.CreateWorkItemAssignedUser(ctx, d, params)
+
+	return err
+}
+
+// TODO: delete from work_item_project_*.
+func (u *WorkItem) Delete(ctx context.Context, d db.DBTX, id int) (*db.WorkItem, error) {
+	workItem, err := u.ByID(ctx, d, id)
+	if err != nil {
+		return nil, fmt.Errorf("could not get workItem: %w", parseErrorDetail(err))
+	}
+
+	err = workItem.SoftDelete(ctx, d)
+	if err != nil {
+		return nil, fmt.Errorf("could not soft delete workItem: %w", parseErrorDetail(err))
+	}
+
+	return workItem, err
+}
+
+// TODO: delete from work_item_project_*.
+func (u *WorkItem) Restore(ctx context.Context, d db.DBTX, id int) (*db.WorkItem, error) {
+	var err error
+	workItem := &db.WorkItem{
+		WorkItemID: id,
+	}
+
+	workItem, err = workItem.Restore(ctx, d)
+	if err != nil {
+		return nil, fmt.Errorf("could not restore workItem: %w", parseErrorDetail(err))
+	}
+
+	return workItem, err
 }
