250a637557cfa82cd9d0823383d36b9f3d8e5310
Daniel Castro <danicc097@gmail.com>
2023-12-02 16:45:11 +0000

    parse underlying index expressions generics

--- cmd/ast-parser/main.go
+++ cmd/ast-parser/main.go
@@ -4,14 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"go/ast"
-	"go/importer"
 	"go/parser"
 	"go/token"
 	"go/types"
 	"log"
 	"os"
+	"path/filepath"
+	"sort"
 	"strings"
 
+	"golang.org/x/exp/maps"
 	"golang.org/x/tools/go/packages"
 )
 
@@ -27,55 +29,64 @@ var (
 	}
 )
 
-func mustParse(fset *token.FileSet, input string) *ast.File {
-	f, err := parser.ParseFile(fset, "", input, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return f
-}
-
-func parseStructs(path string) ([]string, error) {
+func parseStructs(filepath string) ([]string, error) {
 	var sts []string
 
-	fset := token.NewFileSet()
-	inputBlob, err := os.ReadFile(path)
-	if err != nil {
-		log.Fatalf("os.ReadFile: %s", err)
+	loadConfig := &packages.Config{
+		Fset: token.NewFileSet(),
+		Mode: loadMode,
+		ParseFile: func(fset *token.FileSet, filename string, src []byte) (*ast.File, error) {
+			if strings.Contains(filename, filepath) {
+				fmt.Printf("parsing file: %v\n", filename)
+
+				// default behavior. could speed up even more when parsing a directory by ignoring function bodies, etc.
+				const mode = parser.AllErrors | parser.ParseComments
+				return parser.ParseFile(fset, filename, src, mode)
+			}
+
+			return nil, nil
+		},
 	}
-	astFile := mustParse(fset, string(inputBlob))
-	info := types.Info{
-		// Types: make(map[ast.Expr]types.TypeAndValue),
-		Defs: make(map[*ast.Ident]types.Object),
-		// Uses:  make(map[*ast.Ident]types.Object),
+	pkgs, err := packages.Load(loadConfig, "file="+filepath)
+	if err != nil {
+		panic(err)
 	}
 
-	// cfg := &packages.Config{
-	// 	Mode: packages.NeedTypes | packages.NeedSyntax,
-	// }
-	// _, err = packages.Load(cfg, "github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/models")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	for _, pkg := range pkgs {
+		for _, syn := range pkg.Syntax {
+			for _, dec := range syn.Decls {
+				if gen, ok := dec.(*ast.GenDecl); ok && gen.Tok == token.TYPE {
+					for _, spec := range gen.Specs {
+						if ts, ok := spec.(*ast.TypeSpec); ok {
+							obj, ok := pkg.TypesInfo.Defs[ts.Name]
+							if !ok {
+								continue
+							}
+							if _, ok := obj.Type().Underlying().(*types.Struct); ok {
+								if (obj.Exported() && privateOnly) || (!obj.Exported() && publicOnly) {
+									continue
+								}
 
-	// FIXME: super slow due to imports in file itself but works.
-	conf := types.Config{
-		IgnoreFuncBodies:         true,
-		DisableUnusedImportCheck: true,
-		Importer:                 importer.ForCompiler(fset, "source", nil),
-	}
-	_, err = conf.Check("rest", fset, []*ast.File{astFile}, &info)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for ident, obj := range info.Defs {
-		if tn, ok := obj.(*types.TypeName); ok {
-			if _, ok := tn.Type().Underlying().(*types.Struct); ok {
-				if (ident.IsExported() && privateOnly) || (!ident.IsExported() && publicOnly) {
-					continue
+								sts = append(sts, obj.Name())
+							}
+							// typeName, ok := obj.(*types.TypeName)
+							// if !ok {
+							// 	continue
+							// }
+							// named, ok := typeName.Type().(*types.Named)
+							// if !ok {
+							// 	continue
+							// }
+							// // print the full name of the type
+							// // fmt.Println(named)
+
+							// _, ok = named.Underlying().(*types.Struct)
+							// if !ok {
+							// 	continue
+							// }
+						}
+					}
 				}
-				sts = append(sts, ident.Name)
-				// fmt.Printf("  Defined at: %s\n", fset.Position(ident.Pos()))
 			}
 		}
 	}
@@ -89,6 +100,7 @@ const loadMode = packages.NeedName |
 	packages.NeedImports |
 	packages.NeedDeps |
 	packages.NeedTypes |
+	packages.NeedSyntax |
 	packages.NeedSyntax |
 	packages.NeedTypesInfo
 
@@ -115,63 +127,33 @@ func main() {
 	case "find-interfaces":
 		os.Exit(0)
 	case "find-structs":
-		loadConfig := &packages.Config{
-			Fset: token.NewFileSet(),
-			Mode: loadMode,
-			ParseFile: func(fset *token.FileSet, filename string, src []byte) (*ast.File, error) {
-				if strings.Contains(filename, cmd.Args()[0]) {
-					fmt.Printf("parsing file: %v\n", filename)
-
-					// default behavior. could speed up even more when parsing a directory by ignoring function bodies, etc.
-					const mode = parser.AllErrors | parser.ParseComments
-					return parser.ParseFile(fset, filename, src, mode)
+		structs := map[string]struct{}{}
+		for _, filename := range cmd.Args() {
+			err := filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
 				}
-
-				return nil, nil
-			},
-		}
-		pkgs, err := packages.Load(loadConfig, "file="+cmd.Args()[0])
-		if err != nil {
-			panic(err)
-		}
-
-		for _, pkg := range pkgs {
-			for _, syn := range pkg.Syntax {
-				for _, dec := range syn.Decls {
-					fmt.Printf("dec: %v\n", dec)
-					if gen, ok := dec.(*ast.GenDecl); ok && gen != nil && gen.Tok == token.TYPE {
-						// print doc comment of the type
-						if gen.Doc != nil {
-							fmt.Println(gen.Doc.List[0])
-						}
-						for _, spec := range gen.Specs {
-							if ts, ok := spec.(*ast.TypeSpec); ok {
-								obj, ok := pkg.TypesInfo.Defs[ts.Name]
-								if !ok {
-									continue
-								}
-								typeName, ok := obj.(*types.TypeName)
-								if !ok {
-									continue
-								}
-								named, ok := typeName.Type().(*types.Named)
-								if !ok {
-									continue
-								}
-								// print the full name of the type
-								fmt.Println(named)
-
-								_, ok = named.Underlying().(*types.Struct)
-								if !ok {
-									continue
-								}
-
-								fmt.Printf("s.String(): %v\n", named.Obj().Name())
-							}
-						}
+				if !info.IsDir() && filepath.Ext(path) == ".go" && filepath.Base(path) != "_test.go" {
+					sts, err := parseStructs(path)
+					if err != nil {
+						return fmt.Errorf("parseStructs: %w", err)
+					}
+					for _, st := range sts {
+						structs[st] = struct{}{}
 					}
 				}
+
+				return nil
+			})
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
+		sts := maps.Keys(structs)
+		sort.Slice(sts, func(i, j int) bool {
+			return sts[i] < sts[j]
+		})
+		fmt.Printf("sts: %v\n", sts)
+		os.Exit(0)
 	}
 }
