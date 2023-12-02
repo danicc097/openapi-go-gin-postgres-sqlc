80774ecf3e81ceb58e5d5e85bd2de9871074cefc
Daniel Castro <danicc097@gmail.com>
2023-12-02 17:12:44 +0000

    handle patterns and dirs

--- cmd/ast-parser/main.go
+++ cmd/ast-parser/main.go
@@ -12,11 +12,13 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
+	"sync"
 
 	"golang.org/x/exp/maps"
 	"golang.org/x/tools/go/packages"
 )
 
+//nolint:gochecknoglobals
 var (
 	privateOnly, publicOnly bool
 
@@ -27,9 +29,13 @@ var (
 		structsCmd.Name():    structsCmd,
 		interfacesCmd.Name(): interfacesCmd,
 	}
+
+	wg sync.WaitGroup
 )
 
-func parseStructs(filepath string) ([]string, error) {
+func parseStructs(filepath string, resultCh chan<- []string, errCh chan<- error) {
+	defer wg.Done()
+
 	var sts []string
 
 	loadConfig := &packages.Config{
@@ -39,7 +45,6 @@ func parseStructs(filepath string) ([]string, error) {
 			if strings.Contains(filename, filepath) {
 				fmt.Printf("parsing file: %v\n", filename)
 
-				// default behavior. could speed up even more when parsing a directory by ignoring function bodies, etc.
 				const mode = parser.AllErrors | parser.ParseComments
 				return parser.ParseFile(fset, filename, src, mode)
 			}
@@ -47,9 +52,11 @@ func parseStructs(filepath string) ([]string, error) {
 			return nil, nil
 		},
 	}
+
 	pkgs, err := packages.Load(loadConfig, "file="+filepath)
 	if err != nil {
-		panic(err)
+		errCh <- err
+		return
 	}
 
 	for _, pkg := range pkgs {
@@ -69,21 +76,6 @@ func parseStructs(filepath string) ([]string, error) {
 
 								sts = append(sts, obj.Name())
 							}
-							// typeName, ok := obj.(*types.TypeName)
-							// if !ok {
-							// 	continue
-							// }
-							// named, ok := typeName.Type().(*types.Named)
-							// if !ok {
-							// 	continue
-							// }
-							// // print the full name of the type
-							// // fmt.Println(named)
-
-							// _, ok = named.Underlying().(*types.Struct)
-							// if !ok {
-							// 	continue
-							// }
 						}
 					}
 				}
@@ -91,7 +83,7 @@ func parseStructs(filepath string) ([]string, error) {
 		}
 	}
 
-	return sts, nil
+	resultCh <- sts
 }
 
 const loadMode = packages.NeedName |
@@ -127,33 +119,65 @@ func main() {
 	case "find-interfaces":
 		os.Exit(0)
 	case "find-structs":
-		structs := map[string]struct{}{}
-		for _, filename := range cmd.Args() {
-			err := filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+		resultCh := make(chan []string)
+		errCh := make(chan error)
+
+		for _, pattern := range cmd.Args() {
+			matches, err := filepath.Glob(pattern)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, filename := range matches {
+				fileInfo, err := os.Stat(filename)
 				if err != nil {
-					return err
+					log.Fatalf("os.Stat: %s", err)
 				}
-				if !info.IsDir() && filepath.Ext(path) == ".go" && filepath.Base(path) != "_test.go" {
-					sts, err := parseStructs(path)
+
+				if fileInfo.IsDir() {
+					err := filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+						if err != nil {
+							return err
+						}
+						if !info.IsDir() && filepath.Ext(path) == ".go" && filepath.Base(path) != "_test.go" {
+							wg.Add(1)
+							go parseStructs(path, resultCh, errCh)
+						}
+
+						return nil
+					})
 					if err != nil {
-						return fmt.Errorf("parseStructs: %w", err)
+						log.Fatal(err)
 					}
+				} else {
+					wg.Add(1)
+					go parseStructs(filename, resultCh, errCh)
+				}
+
+				go func() {
+					wg.Wait()
+					close(resultCh)
+					close(errCh)
+				}()
+
+				structs := map[string]struct{}{}
+				for sts := range resultCh {
 					for _, st := range sts {
 						structs[st] = struct{}{}
 					}
 				}
 
-				return nil
-			})
-			if err != nil {
-				log.Fatal(err)
+				if len(errCh) > 0 {
+					err := <-errCh
+					log.Fatal(err)
+				}
+
+				sts := maps.Keys(structs)
+				sort.Slice(sts, func(i, j int) bool {
+					return sts[i] < sts[j]
+				})
+				fmt.Printf("sts: %v\n", sts)
+				os.Exit(0)
 			}
 		}
-		sts := maps.Keys(structs)
-		sort.Slice(sts, func(i, j int) bool {
-			return sts[i] < sts[j]
-		})
-		fmt.Printf("sts: %v\n", sts)
-		os.Exit(0)
 	}
 }
