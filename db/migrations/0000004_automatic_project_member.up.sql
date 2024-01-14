8f57d407bf0ad8ddff2b854f2e9bfa61ebf8675d
Daniel Castro <danicc097@gmail.com>
2024-01-14 12:01:04 +0000

    working on dynamic mat table creation

--- db/migrations/0000004_automatic_project_member.up.sql
+++ db/migrations/0000004_automatic_project_member.up.sql
@@ -147,83 +147,137 @@ create trigger sync_user_projects
   after insert or update on user_team for each row
   execute function sync_user_projects ();
 
-
-CREATE OR REPLACE FUNCTION create_dynamic_table(project_name text)
-RETURNS VOID AS $$
-DECLARE
-    project_table_cols text;
-    work_items_cols text;
-BEGIN
-    -- Dynamically fetch column names and data types from work_items
-    EXECUTE '
+create or replace function create_dynamic_table (project_name text)
+  returns VOID
+  as $$
+declare
+  project_table_col_and_type text;
+  work_items_col_and_type text;
+begin
+  -- Dynamically fetch column names and data types from work_items
+  execute '
         SELECT string_agg(column_name || '' '' || data_type, '', '')
         FROM information_schema.columns
-        WHERE table_name = ''work_items'' AND table_schema = ''public'''
-    INTO work_items_cols;
+        WHERE table_name = ''work_items'' AND table_schema = ''public''' into work_items_col_and_type;
 
-    project_table_cols := project_table_cols || ',';
+  project_table_col_and_type := project_table_col_and_type || ',';
 
-    EXECUTE format('
+  execute FORMAT('
         SELECT string_agg(column_name || '' '' || data_type, '', '')
         FROM information_schema.columns
-        WHERE table_name = ''%I'' AND table_schema = ''public''', project_name)
-    INTO project_table_cols;
+        WHERE table_name = ''%I'' AND table_schema = ''public''' , project_name) into project_table_col_and_type;
+  -- Dynamically create the cache.demo_work_items table
+  execute 'CREATE SCHEMA if not exists cache;';
+  -- EXECUTE 'CREATE TABLE cache.demo_work_items (' || project_table_col_and_type || work_items_col_and_type || ')';
+  raise notice '% fields: %' , project_name , project_table_col_and_type;
+  raise notice 'work_item fields: %' , work_items_col_and_type;
 
-    -- Dynamically create the cache.demo_work_items table
-    EXECUTE 'CREATE SCHEMA if not exists cache;';
-    -- EXECUTE 'CREATE TABLE cache.demo_work_items (' || project_table_cols || work_items_cols || ')';
-    raise notice '% fields: %', project_name, project_table_cols;
-    raise notice 'work_item fields: %', work_items_cols;
+end;
+$$
+language plpgsql;
 
-END;
-$$ LANGUAGE plpgsql;
+select
+  create_dynamic_table ('demo_work_items');
 
-SELECT create_dynamic_table('demo_work_items');
+create or replace function sync_work_items ()
+  returns trigger
+  as $$
+declare
+  project_name text;
+  project_table_cols text[];
+  work_items_cols text[];
+  sync_cols text[];
+  update_cols text[];
+begin
+  project_name := TG_ARGV[0];
+  -- Make sure the project table exists
+  perform
+    1
+  from
+    pg_catalog.pg_class c
+    join pg_catalog.pg_namespace n on n.oid = c.relnamespace
+  where
+    n.nspname = 'public'
+    and c.relname = project_name
+    and c.relkind = 'r';
 
-
-CREATE OR REPLACE FUNCTION sync_work_items()
-RETURNS TRIGGER AS $$
-DECLARE
-    project_name text;
-    columns text;
-BEGIN
-    -- Retrieve the project name from TG_ARGV
-    project_name := TG_ARGV[0];
-
-    -- Check if the project table exists
-    PERFORM 1
-    FROM pg_catalog.pg_class c
-    JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-    WHERE n.nspname = 'public'
-      AND c.relname = project_name
-      AND c.relkind = 'r';
-
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'Project table "%" does not exist', project_name;
-    END IF;
-
-    -- Dynamically fetch the column names from the project table
-    EXECUTE format('
-        SELECT string_agg(column_name, '', '')
+  if not FOUND then
+    raise exception 'Project table "%" does not exist' , project_name;
+  end if;
+  -- Dynamically fetch column names
+  execute FORMAT('
+        SELECT ARRAY_AGG(column_name)
         FROM information_schema.columns
-        WHERE table_name = ''%I'' AND table_schema = ''public''', project_name) INTO columns;
+        WHERE table_name = ''%I'' AND table_schema = ''public''' , project_name) into project_table_cols;
+  execute '
+        SELECT ARRAY_AGG(column_name)
+        FROM information_schema.columns
+        WHERE table_name = ''work_items'' AND table_schema = ''public''' into work_items_cols;
 
-    raise notice 'coluumns: %', columns;
+  raise notice 'work_items_cols: %' , work_items_cols;
+  -- Construct the list of columns to synchronize
+  sync_cols := array (
+    select
+      'wi.' || column_name || ' AS ' || column_name
+    from
+      UNNEST(work_items_cols) as column_name) || array (
+    select
+      'new.' || column_name || ' AS ' || column_name
+    from
+      UNNEST(project_table_cols) as column_name);
+  -- Construct the list of columns for the ON CONFLICT DO UPDATE part
+  update_cols := array (
+    select
+      column_name || ' = EXCLUDED.' || column_name
+    from
+      UNNEST(project_table_cols) as column_name) || array (
+    select
+      column_name || ' = wi.' || column_name
+    from
+      UNNEST(work_items_cols) as column_name);
 
-    -- Dynamically execute the synchronization query
-    raise notice 'Would have updated cache: %', format('
+  -- TODO: should exit early on clashing column names.
+  -- need a trigger like post-migration/2-check-projects.sql so its catched in development
+  -- Dynamically execute the synchronization query
+  raise notice 'insert stmt: %' , FORMAT('
         INSERT INTO cache.%I (%s)
         SELECT %s
-        FROM public.work_items
-        -- TODO: ON CONFLICT work_item_id UPDATE SET
-    ', project_name, columns, columns);
+        FROM %I
+        JOIN work_items wi USING (work_item_id)
+        ON CONFLICT (work_item_id) DO UPDATE
+        SET %s
+    ' , project_name ,
+    -- Concatenate all column names in order
+    ARRAY_TO_STRING(array (
+        select
+          UNNEST(project_table_cols)
+      union
+      select
+        UNNEST(work_items_cols)) , ',') ,
+    -- Constructing SELECT clause with wi. and NEW. prefixes
+    ARRAY_TO_STRING(sync_cols , ', ') , project_name ,
+    -- Constructing SET clause for conflict resolution
+    ARRAY_TO_STRING(update_cols , ', '));
 
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+  return NEW;
+end;
+$$
+language plpgsql;
 
+create trigger work_items_sync_trigger
+  after insert or update on demo_work_items for each row
+  execute function sync_work_items ('demo_work_items');
 
-CREATE TRIGGER work_items_sync_trigger
-AFTER INSERT OR UPDATE ON demo_work_items
-FOR EACH ROW
-EXECUTE FUNCTION sync_work_items('demo_work_items');
+-- INSERT INTO cache.demo_work_items (work_item_id,ref,line,last_message_at,reopened, <here would go work_items_cols>)
+-- SELECT wi.work_item_id AS work_item_id, wi.ref AS ref, wi.line AS line, wi.last_message_at AS last_message_at, wi.reopened AS reopened
+-- FROM demo_work_items wi
+-- JOIN work_items nw ON wi.work_item_id = nw.work_item_id
+-- ON CONFLICT (work_item_id) DO UPDATE
+-- SET
+-- work_item_id = EXCLUDED.work_item_id,
+-- ref = EXCLUDED.ref,
+-- line = EXCLUDED.line,
+-- last_message_at = EXCLUDED.last_message_at,
+-- reopened = EXCLUDED.reopened
+-- ,
+-- <here would go work_items_cols doing <col> = wi.<col>>
