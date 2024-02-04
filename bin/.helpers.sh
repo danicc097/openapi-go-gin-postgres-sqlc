70f37950140b724d893a1cfcb713a84de1310594
Daniel Castro <danicc097@gmail.com>
2024-02-04 09:59:55 +0100

    easy caching of go bin tools

--- bin/.helpers.sh
+++ bin/.helpers.sh
@@ -411,6 +411,7 @@ cache_all() {
 
   if cmp -s "$output_file" "$output_file.tmp" && [[ $X_FORCE_REGEN -eq 0 ]]; then
     echo "${CYAN}Skipping generation (cached).${OFF} Regenerate with ${RED}--x-force-regen${OFF}"
+    rm "$output_file.tmp"
     return 0
   fi
 
