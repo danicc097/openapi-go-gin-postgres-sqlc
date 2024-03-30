fcba9179d9f93a15eaa537104f056e79a2696f3e
Daniel Castro <danicc097@gmail.com>
2024-03-30 12:19:51 +0100

    update filters

--- internal/repos/postgresql/filters.go
+++ internal/repos/postgresql/filters.go
@@ -9,50 +9,78 @@ import (
 	"github.com/danicc097/openapi-go-gin-postgres-sqlc/internal/repos/postgresql/gen/db"
 )
 
-func GenerateFilters(entity db.TableEntity, queryParams map[string][]string) (map[string][]interface{}, error) {
+const (
+	ilike = " ILIKE $i"
+	equal = " = $i"
+
+	// https://www.postgresql.org/docs/16/functions-array.html#ARRAY-OPERATORS-TABLE
+	// https://www.postgresql.org/docs/16/functions-subquery.html
+	// TODO: should allow pagination with array filters on db fields that are not arrays,
+	// e.g. find documents with the given refs, which the user enters in the combobox separated by ;
+	// and we generate all combobox options (separator customizable)
+	// this can't be done with default text filtering.
+	// not to be confused with filtering on array columns which isn't implemented at the moment
+)
+
+func GenerateFilters(entity db.TableEntity, queryParams map[string]models.PaginationFilter) (map[string][]interface{}, error) {
 	filters := make(map[string][]interface{})
 
-	for key, values := range queryParams {
+	for id, filter := range queryParams {
 		if _, ok := db.EntityFilters[entity]; !ok {
 			return nil, fmt.Errorf("invalid entity: %v", entity)
 		}
-		filter, ok := db.EntityFilters[entity][key]
+		dbfilter, ok := db.EntityFilters[entity][id]
 		if !ok {
 			continue
 		}
 
-		for _, value := range values {
-			switch filter.Type {
+		disc, err := filter.Value.Discriminator()
+		if err != nil {
+			return nil, fmt.Errorf("discriminator: %w", err)
+		}
+		filterMode := models.PaginationFilterModes(disc)
+		v, _ := filter.Value.ValueByDiscriminator()
+		switch t := v.(type) {
+		case models.PaginationFilterArrayValue:
+			// we can have arrincludessome, arrincludesall, arrincludes.
+			// will not share modes with single values.
+		case models.PaginationFilterSingleValue:
+			if t.Value == nil {
+				continue
+			}
+			v := *t.Value
+			switch dbfilter.Type {
 			case "string":
-				switch models.PaginationFilterModes(value) {
+				switch filterMode {
 				case models.PaginationFilterModesEquals:
-					filters[fmt.Sprintf("%s = ?", filter.Db)] = []interface{}{value}
+					filters[dbfilter.Db+equal] = []interface{}{v}
 				case models.PaginationFilterModesContains:
-					filters[fmt.Sprintf("%s ILIKE ?", filter.Db)] = []interface{}{"%" + value + "%"}
+					filters[dbfilter.Db+ilike] = []interface{}{"%" + v + "%"}
 				case models.PaginationFilterModesStartsWith:
-					filters[fmt.Sprintf("%s ILIKE ?", filter.Db)] = []interface{}{value + "%"}
+					filters[dbfilter.Db+ilike] = []interface{}{v + "%"}
 				case models.PaginationFilterModesEndsWith:
-					filters[fmt.Sprintf("%s ILIKE ?", filter.Db)] = []interface{}{"%" + value}
+					filters[dbfilter.Db+ilike] = []interface{}{"%" + v}
 				}
 			case "integer":
-				if intValue, err := strconv.Atoi(value); err == nil {
-					filters[fmt.Sprintf("%s = ?", filter.Db)] = []interface{}{intValue}
+				if intValue, err := strconv.Atoi(v); err == nil {
+					filters[dbfilter.Db+equal] = []interface{}{intValue}
 				}
 			case "float":
-				if floatValue, err := strconv.ParseFloat(value, 64); err == nil {
-					filters[fmt.Sprintf("%s = ?", filter.Db)] = []interface{}{floatValue}
+				if floatValue, err := strconv.ParseFloat(v, 64); err == nil {
+					filters[dbfilter.Db+equal] = []interface{}{floatValue}
 				}
 			case "date-time":
-				if dateTimeValue, err := time.Parse(time.RFC3339, value); err == nil {
-					filters[fmt.Sprintf("%s = ?", filter.Db)] = []interface{}{dateTimeValue}
+				if dateTimeValue, err := time.Parse(time.RFC3339, v); err == nil {
+					filters[dbfilter.Db+equal] = []interface{}{dateTimeValue}
 				}
 			case "boolean":
 				// we will receive actual types (boolean, time.Time) via runtime package
-				if value == "true" || value == "false" {
-					boolValue := value == "true"
-					filters[fmt.Sprintf("%s = ?", filter.Db)] = []interface{}{boolValue}
+				if v == "true" || v == "false" {
+					filters[dbfilter.Db+equal] = []interface{}{v == "true"}
 				}
 			}
+		default:
+			return nil, fmt.Errorf("unsupported filter mode type: %v", t)
 		}
 	}
 
