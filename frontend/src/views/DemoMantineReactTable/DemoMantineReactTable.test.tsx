db0bfa8617f0a00539be2f8db0e5f9e247567ded
Daniel Castro <danicc097@gmail.com>
2024-04-12 19:12:03 +0200

    debug ci

--- frontend/src/views/DemoMantineReactTable/DemoMantineReactTable.test.tsx
+++ frontend/src/views/DemoMantineReactTable/DemoMantineReactTable.test.tsx
@@ -1,10 +1,12 @@
 import { faker } from '@faker-js/faker'
+import axios from 'axios'
 import dayjs from 'dayjs'
 import { HttpResponse, http } from 'msw'
 import { setupServer } from 'msw/node'
 import { UserID } from 'src/gen/entity-ids'
 import { PaginatedUsersResponse, User } from 'src/gen/model'
 import { getGetPaginatedUsersMockHandler } from 'src/gen/user/user.msw'
+import { apiPath } from 'src/services/apiPaths'
 import { render, screen } from 'src/test-utils'
 import DemoMantineReactTable from 'src/views/DemoMantineReactTable/DemoMantineReactTable'
 
@@ -29,7 +31,6 @@ function usersForPage(page: number): User[] {
   }))
 }
 
-const server = setupServer()
 const firstPage: PaginatedUsersResponse = {
   items: usersForPage(1),
   page: {
@@ -42,23 +43,37 @@ const secondPage: PaginatedUsersResponse = {
     nextCursor: 'next-cursor-2',
   },
 }
+
+console.log('debug 01')
+const server = setupServer()
+console.log('debug 02')
 // establish API mocking before all tests
 beforeAll(() => server.listen())
+console.log('debug 03')
 // reset any request handlers that are declared as a part of our tests
 // (i.e. for testing one-time error scenarios)
+console.log('debug 04')
 afterEach(() => server.resetHandlers())
 // clean up once the tests are done
+console.log('debug 05')
 afterAll(() => server.close())
+console.log('debug 0')
 
-test('Renders content', async () => {
+test('mrt-table-tests-render', async () => {
   render(<DemoMantineReactTable></DemoMantineReactTable>)
 
+  console.log('debug 1')
   server.use(getGetPaginatedUsersMockHandler(firstPage))
 
+  console.log('debug 2')
+  const p = apiPath()
+  console.log(p)
+  const res = await (await axios.get(`https://test.com/user/page`)).data
+  console.log({ res })
   console.log({ handlers: server.listHandlers() })
   console.log(document.body.innerHTML)
 
-  const el = await screen.findByText(firstPage.items![0]!.email, {}, { timeout: 50000 })
+  const el = await screen.findByText(firstPage.items![0]!.email, {}, { timeout: 5000 })
   const allRows = screen.queryAllByRole('row')
   const firstRow = allRows.filter((row) => row.getAttribute('data-index') === '0')
 
