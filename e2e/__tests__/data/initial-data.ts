4fe329a76981a7cdc60f813512fbf6ff18d31d60
Daniel Castro <danicc097@gmail.com>
2024-04-24 17:26:43 +0200

    notes

--- e2e/__tests__/data/initial-data.ts
+++ e2e/__tests__/data/initial-data.ts
@@ -3,6 +3,10 @@ import type * as models from "client/gen/model";
 //////////
 // source: models.go
 
+/**
+ * User ids are uuids, therefore we can use any unique column for
+ * e2e identifiers, which is also easier to reason about.
+ */
 export interface User {
   username: string;
   email: string;
@@ -11,6 +15,10 @@ export interface User {
   scopes: models.Scopes;
   role: models.Role;
 }
+/**
+ * TODO: should include ids for the rest of entities with serial ids,
+ * given that e2e data will not be created concurrently.
+ */
 export interface Team {
   name: string;
   projectName: models.Project;
